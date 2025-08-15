d4251b22b2a59f57a8f2a45d52b71382f7920c42
Morialle Barr <Moriallekbarr@gmail.com>
2025-08-15 10:45:27 +0300

    TimerFix_v1

--- Assets/Scripts/PopupQuest.cs
+++ Assets/Scripts/PopupQuest.cs
@@ -9,11 +9,25 @@ public class PopupQuest : MonoBehaviour
     [SerializeField] private Text _timerText;
     [SerializeField] private RewardContainer _rewardContainer;
     [SerializeField] private Stage[] _stages;
-    
+
     void Start()
     {
-        _missionHeader.text = $"Level {Random.Range(1, 15)} \"UNBEATABLE CHAMP\"";
+        _missionHeader.text = $"LEVEL {Random.Range(1, 15)} \"UNBEATABLE CHAMP\"";
+        StartCoroutine(Countdown());
     }
 
-
+    IEnumerator Countdown()
+    {
+        // 23 hours, 59 minutes, 59 seconds
+        int totalSeconds = 23 * 3600 + 59 * 60 + 59;
+        while (totalSeconds >= 0)
+        {
+            int h = totalSeconds / 3600;
+            int m = (totalSeconds % 3600) / 60;
+            int s = totalSeconds % 60;
+            _timerText.text = $"{h:00}:{m:00}:{s:00}";
+            yield return new WaitForSeconds(1f);
+            totalSeconds--;
+        }
+    }
 }
